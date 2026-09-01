7c28ca4941438ecda5c5bbeba67f791924b84491
Fryed Biscuit <fryedbiscuit@gmail.com>
2026-09-01 16:20:12 -0300

    Tue Sep  1 16:20:16 -03 2026

--- common/dot-config/nvim/lua/config/keymaps.lua
+++ common/dot-config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,29 @@
 local nest = require("nest")
+local nest = require("nest")
+
+local function toggle_loclist()
+  local is_open = vim.fn.getloclist(0, { winid = 0 }).winid ~= 0
+
+  if is_open then
+    vim.cmd("lclose")
+  elseif #vim.fn.getloclist(0) > 0 then
+    vim.cmd("lopen")
+  else
+    vim.notify("Location list is empty", vim.log.levels.WARN)
+  end
+end
 
 nest.applyKeymaps {
 	{ ",", 
 		{ "c", "<cmd>tabnew<cr><cmd>e ~/.config/nvim/init.lua<cr>" },
+		{ "k", "<cmd>tabnew<cr><cmd>e ~/.config/nvim/lua/config/keymaps.lua<cr>" },
 	},
+
+	--Location list
+	{ "<C-l><C-l>", toggle_loclist},
+	{ "<C-j>", "<cmd>lnext<CR>", options = { desc = "Next location list item", silent = true } },
+	{ "<C-k>", "<cmd>lprev<CR>", options = { desc = "Previous location list item", silent = true } },
+	{ '<leader>cd', "<cmd>CocDiagnostics<cr>"},
 
 	-- Make
 	{ "<leader>m", {
