d9dcfa48452f166d566ea2c9b15ab7c357187c5c
Fryed Biscuit <fryedbiscuit@gmail.com>
2026-09-02 14:41:06 -0300

    a

--- common/dot-config/nvim/lua/plugins/oil.lua
+++ common/dot-config/nvim/lua/plugins/oil.lua
@@ -2,15 +2,20 @@ return {
 	{
 	'stevearc/oil.nvim',
 	---@module 'oil'
-	---@type oil.SetupOpt s
+	---@type oil.SetupOpts
 	opts = {},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 	config = function () 
 		require("oil").setup({
 			default_file_explorer = true,
 			columns = {
 				"icon",
 				-- "permissions",
-				-- "size",
+				"size",
 				-- "mtime",
 			},
 			-- Buffer-local options to use for oil buffers
@@ -199,11 +204,6 @@ return {
 			},
 		})
 	end,
-	-- Optional dependencies
-	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	lazy = true
-	},
 }
 
+}
